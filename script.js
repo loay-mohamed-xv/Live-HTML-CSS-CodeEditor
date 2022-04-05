e35f21d6963c6b3a4f04939653743d9410b95ee1
Loay Mohamed <72279810+loay-mohamed-xv@users.noreply.github.com>
2022-04-05 02:24:10 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function compileHTML(field, e){
+    document.getElementById('output').innerHTML = field.value;
+}
+
+function compileCSS(field, e){
+    document.getElementById('style').innerHTML = field.value;
+}
